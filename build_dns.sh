87d298e64d1afe932ffdedaf922bc405f4283bfb
Charlie Porth <charlieporth1@gmail.com>
2022-11-14 15:39:24 -0600

    First commit with ts

--- build_dns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt -y install golang git
-git clone https://github.com/folbricht/routedns
-cd routedns/cmd/routedns
-go build -v .
-export ROUTEDNS_DIR=$PWD
--- start_dns.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CONFIG_DIR=routedns
-source ./build_dns.sh
-$ROUTEDNS_DIR/routedns --log-level=5 $CONFIG_DIR/*.toml
